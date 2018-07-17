c972c51872cc3b53546e7706c01ed72256936038
Jagannath <pandeyajagannath@gmail.com>
2018-07-17 19:02:13 +0530

    hi

---
+++ Script/Script/ExcelUtility.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace Script
+{
+    public static class ExcelUtility
+    {
+
+        public static void ExportDataSetToExcel(DataSet ds)
+        {
+            //Creae an Excel application instance
+            Excel.Application excelApp = new Excel.Application();
+
+            //Create an Excel workbook instance and open it from the predefined location
+          
+            Excel.Workbook excelWorkBook = excelApp.Workbooks.Open(@"d:\Reliant.xlsx");
+
+            foreach (DataTable table in ds.Tables)
+            {
+                //Add a new worksheet to workbook with the Datatable name
+                Excel.Worksheet excelWorkSheet = excelWorkBook.Sheets.Add();
+                excelWorkSheet.Name = table.TableName;
+
+                for (int i = 1; i < table.Columns.Count + 1; i++)
+                {
+                    excelWorkSheet.Cells[1, i] = table.Columns[i - 1].ColumnName;
+                }
+
+                for (int j = 0; j < table.Rows.Count; j++)
+                {
+                    for (int k = 0; k < table.Columns.Count; k++)
+                    {
+                        excelWorkSheet.Cells[j + 2, k + 1] = table.Rows[j].ItemArray[k].ToString();
+                    }
+                }
+            }
+
+            excelWorkBook.Save();
+            excelWorkBook.Close();
+            excelApp.Quit();
+
+        }
+        public static bool WriteDataTableToExcel(System.Data.DataTable dataTable, string saveAsLocation)
+        {
+            Microsoft.Office.Interop.Excel.Application excel;
+            Microsoft.Office.Interop.Excel.Workbook excelworkBook;
+            Microsoft.Office.Interop.Excel.Worksheet excelSheet;
+            Microsoft.Office.Interop.Excel.Range excelCellrange;
+
+            try
+            {
+                string str = Environment.CurrentDirectory + "\\" + saveAsLocation;
+                if (!File.Exists(str))
+                {
+                    File.Create(str).Dispose();
+                }
+                // Start Excel and get Application object.
+                excel = new Microsoft.Office.Interop.Excel.Application();
+
+                // for making Excel visible
+                excel.Visible = false;
+                excel.DisplayAlerts = false;
+
+                // Creation a new Workbook
+                excelworkBook = excel.Workbooks.Add(Type.Missing);
+
+                // Workk sheet
+                excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)excelworkBook.ActiveSheet;
+                excelSheet.Name = "Report";
+
+
+                excelSheet.Cells[1, 1] = "Details";
+                excelSheet.Cells[1, 2] = "Date : " + DateTime.Now.ToShortDateString();
+
+                // loop through each row and add values to our sheet
+                int rowcount = 2;
+
+                foreach (DataRow datarow in dataTable.Rows)
+                {
+                    rowcount += 1;
+                    for (int i = 1; i <= dataTable.Columns.Count; i++)
+                    {
+                        // on the first iteration we add the column headers
+                        if (rowcount == 3)
+                        {
+                            excelSheet.Cells[2, i] = dataTable.Columns[i - 1].ColumnName;
+                            excelSheet.Cells.Font.Color = System.Drawing.Color.Black;
+
+                        }
+
+                        excelSheet.Cells[rowcount, i] = datarow[i - 1].ToString();
+
+                        //for alternate rows
+                        if (rowcount > 3)
+                        {
+                            if (i == dataTable.Columns.Count)
+                            {
+                                if (rowcount % 2 == 0)
+                                {
+                                    excelCellrange = excelSheet.Range[excelSheet.Cells[rowcount, 1], excelSheet.Cells[rowcount, dataTable.Columns.Count]];
+                                    FormattingExcelCells(excelCellrange, "#CCCCFF", System.Drawing.Color.Black, false);
+                                }
+
+                            }
+                        }
+
+                    }
+
+                }
+
+                // now we resize the columns
+                excelCellrange = excelSheet.Range[excelSheet.Cells[1, 1], excelSheet.Cells[rowcount, dataTable.Columns.Count]];
+                excelCellrange.EntireColumn.AutoFit();
+                Microsoft.Office.Interop.Excel.Borders border = excelCellrange.Borders;
+                border.LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+                border.Weight = 2d;
+
+
+                excelCellrange = excelSheet.Range[excelSheet.Cells[1, 1], excelSheet.Cells[2, dataTable.Columns.Count]];
+                FormattingExcelCells(excelCellrange, "#000099", System.Drawing.Color.White, true);
+
+
+                //now save the workbook and exit Excel
+
+
+                excelworkBook.SaveAs(str);
+                excelworkBook.Close();
+                excel.Quit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //ErrorLog.LogFile(ex);
+                return false;
+
+            }
+            finally
+            {
+                excelSheet = null;
+                excelCellrange = null;
+                excelworkBook = null;
+            }
+
+        }
+
+        /// <summary>
+        /// FUNCTION FOR FORMATTING EXCEL CELLS
+        /// </summary>
+        /// <param name="range"></param>
+        /// <param name="HTMLcolorCode"></param>
+        /// <param name="fontColor"></param>
+        /// <param name="IsFontbool"></param>
+        public static void FormattingExcelCells(Microsoft.Office.Interop.Excel.Range range, string HTMLcolorCode, System.Drawing.Color fontColor, bool IsFontbool)
+        {
+            range.Interior.Color = System.Drawing.ColorTranslator.FromHtml(HTMLcolorCode);
+            range.Font.Color = System.Drawing.ColorTranslator.ToOle(fontColor);
+            if (IsFontbool == true)
+            {
+                range.Font.Bold = IsFontbool;
+            }
+        }
+        public static bool WriteLog(Exception strMessage, string method)
+        {
+            try
+            {
+                string strFileName = @"d:\\log\log_" + DateTime.Now + ".txt";
+                FileStream objFilestream = new FileStream(string.Format("{0}\\{1}", Path.GetTempPath(), strFileName), FileMode.Append, FileAccess.Write);
+                StreamWriter objStreamWriter = new StreamWriter((Stream)objFilestream);
+                objStreamWriter.WriteLine(strMessage.StackTrace.ToString());
+                objStreamWriter.WriteLine("*************************");
+                objStreamWriter.WriteLine("Method Name" + method);
+                objStreamWriter.Close();
+                objFilestream.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+        public static void ExportToExcel(this DataTable dataTable, String filePath, bool overwiteFile = true)
+        {
+            if (File.Exists(filePath) && overwiteFile)
+            {
+                File.Delete(filePath);
+            }
+
+            var conn = $"Provider=Microsoft.Jet.OLEDB.4.0;Data Source={filePath};Extended Properties='Excel 8.0;HDR=Yes;IMEX=0';";
+            using (OleDbConnection connection = new OleDbConnection(conn))
+            {
+                connection.Open();
+                using (OleDbCommand command = new OleDbCommand())
+                {
+                    command.Connection = connection;
+
+                    List<String> columnNames = new List<string>();
+                    foreach (DataColumn dataColumn in dataTable.Columns)
+                    {
+                        columnNames.Add(dataColumn.ColumnName);
+                    }
+
+                    String tableName = !String.IsNullOrWhiteSpace(dataTable.TableName) ? dataTable.TableName : Guid.NewGuid().ToString();
+                    command.CommandText = $"CREATE TABLE [{tableName}] ({String.Join(",", columnNames.Select(c => $"[{c}] VARCHAR").ToArray())});";
+                    command.ExecuteNonQuery();
+
+
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        List<String> rowValues = new List<string>();
+                        foreach (DataColumn column in dataTable.Columns)
+                        {
+                            rowValues.Add((row[column] != null && row[column] != DBNull.Value) ? row[column].ToString() : String.Empty);
+                        }
+                        command.CommandText = $"INSERT INTO [{tableName}]({String.Join(",", columnNames.Select(c => $"[{c}]"))}) VALUES ({String.Join(",", rowValues.Select(r => $"'{r}'").ToArray())});";
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                connection.Close();
+            }
+        }
+    }
+}
---
+++ Script/Script/Program.cs
@@ -0,0 +1,292 @@
+﻿using Script.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Script
+{
+    public class Result
+    {
+        public string TCM_ID { get; set; }
+        public string Title { get; set; }
+        // public string Item_Type { get; set; }
+        // public string Publication_Id { get; set; }
+        //public string Using_Publication_id { get; set; }
+        public string Using_Publication_name_With_Local { get; set; }
+        public string Using_Publication_name_Without_Local { get; set; }
+        // public string Publish_Status { get; set; }
+
+        public string Using_Component_Tcm_ID { get; set; }
+
+        public string using_Page_tcm_id { get; set; }
+
+
+        //public string LocalCopy { get; set; }
+    }
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            CoreServiceClient Client = new CoreServiceClient("basicHttp");
+            // bluePrintInfo(Client);
+            string str = string.Empty;
+            // str = lookUpPublication("300 Multi-brand Content ES");
+
+            if (listComponent(Client, "test"))
+            {
+                Console.WriteLine("Connected");
+                Console.ReadLine();
+
+            }
+            else
+            {
+
+                Console.WriteLine("not Connected");
+                Console.ReadLine();
+            }
+        }
+
+
+        public static bool bluePrintInfo(CoreServiceClient client)
+        {
+            client.ClientCredentials.Windows.ClientCredential.Domain = ConfigurationSettings.AppSettings["Domain"];// "nrg";
+            client.ClientCredentials.Windows.ClientCredential.UserName = ConfigurationSettings.AppSettings["User"]; //"jpandeya";
+            client.ClientCredentials.Windows.ClientCredential.Password = ConfigurationSettings.AppSettings["PWD"];// "jags*786687";
+            UsingItemsFilterData usingItemsFilterData = new UsingItemsFilterData
+            {
+
+                ItemTypes = new[] { ItemType.PublicationTarget, ItemType.Publication, ItemType.Component, ItemType.Page, ItemType.Keyword }
+             ,
+                IncludeLocalCopies = true,
+                BaseColumns = ListBaseColumns.Extended,
+
+
+            };
+
+            PublicationsFilterData filter = new PublicationsFilterData
+            {
+                BaseColumns = ListBaseColumns.IdAndTitle
+            };
+            XElement publications = client.GetSystemWideListXml(filter);
+
+
+            XElement elist = client.GetListXml("tcm:3-1973", usingItemsFilterData);
+
+            List<Result> lstRS = new List<Result>();
+
+            IEnumerable<XElement> usingXML1 = (from el in elist.Elements()
+                                                   //where (string)el.Attribute("IsPublished").Value == "true"
+                                               select el);
+
+
+            return true;
+        }
+        public static bool listComponent(CoreServiceClient client, string path)
+        {
+            try
+            {
+
+                client.ClientCredentials.Windows.ClientCredential.Domain = ConfigurationSettings.AppSettings["Domain"];// "nrg";
+                client.ClientCredentials.Windows.ClientCredential.UserName = ConfigurationSettings.AppSettings["User"]; //"jpandeya";
+                client.ClientCredentials.Windows.ClientCredential.Password = ConfigurationSettings.AppSettings["PWD"];// 
+
+
+                XElement elist = client.GetListXml(ConfigurationSettings.AppSettings["FolderTCM_ID"].ToString(), new OrganizationalItemItemsFilterData
+                {
+                    ItemTypes = new[] { ItemType.Component },
+                    Recursive = true,
+                    BaseColumns = ListBaseColumns.Default
+                });
+
+                List<Result> lstRS = new List<Result>();
+                System.Data.DataTable dt = new System.Data.DataTable();
+                Stopwatch watch = new Stopwatch();
+                TimeSpan timeSpan = new TimeSpan();
+
+                watch.Start();
+                IEnumerable<XElement> elist1 = (from el in elist.Elements()
+                                                    //where (string)el.Attribute("IsPublished").Value == "true"
+                                                select el);
+                foreach (var x in elist1.ToArray())
+                {
+                    Result rs = new Result();
+                    try
+                    {
+
+
+
+
+
+                        UsingItemsFilterData usingItemsFilterData = new UsingItemsFilterData
+                        {
+
+                            ItemTypes = new[] { ItemType.PublicationTarget, ItemType.Publication, ItemType.Component, ItemType.Page, ItemType.Keyword }
+                              ,
+                            IncludeLocalCopies = true
+                        };
+
+                        XElement usingXML = client.GetListXml(x.FirstAttribute.Value, usingItemsFilterData);
+                        #region Comment
+                        //BluePrintChainFilterData filter = new BluePrintChainFilterData();
+                        //filter.Direction = BluePrintChainDirection.Down;
+                        //XElement result = client.GetListXml(x.FirstAttribute.Value, filter);
+                        //IEnumerable<XElement> result1 = (from el in result.Elements()
+                        //                                     //where (string)el.Attribute("IsPublished").Value == "true"
+                        //                                 select el);
+                        //StringBuilder sbBlueChain = new StringBuilder();
+                        //StringBuilder sbLocal = new StringBuilder();
+                        //foreach (var z in result1.ToArray())
+                        //{
+                        //    sbBlueChain.Append(z.FirstAttribute.Value + " ; ");
+                        //    if (z.Attribute("FromPub") != null)
+                        //    {
+                        //        sbLocal.Append(z.Attribute("FromPub").Value + " ; ");
+                        //    }
+
+                        //}
+                        //rs.Blueprint_Chain_TCM_ID = sbBlueChain.ToString();
+                        // rs.LocalCopy = sbLocal.ToString();
+                        #endregion
+                        IEnumerable<XElement> usingXML1 = (from el in usingXML.Elements()
+                                                           select el);
+                        StringBuilder sbComponent = new StringBuilder();
+                        StringBuilder sbKeyword = new StringBuilder();
+                        StringBuilder sbPage = new StringBuilder();
+                        StringBuilder sbPublicationNameWL = new StringBuilder();
+                        StringBuilder sbPublicationNameWOL = new StringBuilder();
+                        if (usingXML1.ToArray().Count() < 2 && usingXML1.ToArray().Count() != 0)
+                        {
+                            rs.TCM_ID = x.FirstAttribute.Value;
+                            rs.Title = x.Attribute("Title").Value;
+                            foreach (var y in usingXML1.ToArray())
+                            {
+
+                                if (y.Attribute("Type").Value == "16" && y.Attribute("CommentToken") != null)
+                                {
+                                    sbComponent.Append(y.FirstAttribute.Value + " ; ");
+                                    sbPublicationNameWL.Append(y.Attribute("Publication").Value + " ; ");
+                                }
+                                if (y.Attribute("Type").Value == "16" && y.Attribute("CommentToken") == null)
+                                {
+                                    sbComponent.Append(y.FirstAttribute.Value + " ; ");
+                                    sbPublicationNameWOL.Append(y.Attribute("Publication").Value + " ; ");
+                                    #region Demote
+                                    //Demote Logic
+                                    //string strPubID = string.Empty;
+                                    //strPubID = lookUpPublication(y.Attribute("Publication").Value);
+                                    //OperationInstruction instruction = new OperationInstruction { Mode = OperationMode.FailOnError };
+                                    //OperationResultDataOfRepositoryLocalObjectData result = client.Promote(x.FirstAttribute.Value, strPubID, instruction, new ReadOptions());
+
+                                    //if (result.ValidationWarnings.Length > 0)
+                                    //{
+                                    //    Console.WriteLine("Validation warnings:");
+                                    //    foreach (ValidationWarningData warning in result.ValidationWarnings)
+                                    //    {
+                                    //        Console.WriteLine("\tMessage:{0} | Source:{1} | Location:{2}",
+                                    //            warning.Message, warning.Source, warning.Location);
+                                    //    }
+                                    //}
+
+                                    #endregion
+
+                                }
+
+                                //if (y.Attribute("Type").Value == "64")
+                                //{
+                                //    sbPage.Append(y.FirstAttribute.Value + " ; ");
+                                //    sbPublicationNameWL.Append(y.Attribute("Publication").Value + " ; ");
+                                //}
+
+                            }
+                            rs.Using_Component_Tcm_ID = (string.IsNullOrEmpty(sbComponent.ToString())) ? "NA" : sbComponent.ToString();
+                            rs.using_Page_tcm_id = (string.IsNullOrEmpty(sbPage.ToString())) ? "NA" : sbPage.ToString();
+                            // rs.using_keyword_tcm_id = (string.IsNullOrEmpty(sbKeyword.ToString())) ? "NA" : sbKeyword.ToString();
+                            rs.Using_Publication_name_With_Local = (string.IsNullOrEmpty(sbPublicationNameWL.ToString())) ? "NA" : sbPublicationNameWL.ToString();
+                            rs.Using_Publication_name_Without_Local = (string.IsNullOrEmpty(sbPublicationNameWOL.ToString())) ? "NA" : sbPublicationNameWOL.ToString();
+                            if (rs.TCM_ID != null)
+                            {
+                                lstRS.Add(rs);
+                                rs = null;
+
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        ExcelUtility.WriteLog(ex, "elist1");
+                        rs = null;
+                    }
+                    timeSpan = TimeSpan.FromSeconds(Convert.ToInt32(watch.Elapsed.TotalSeconds));
+                    Console.Write(timeSpan.ToString("c"));
+                    Console.Write('\r');
+
+                }
+
+                lstRS = lstRS.AsEnumerable().ToList();
+                dt = ToDataTable(lstRS);
+                dt.TableName = "REPORT";
+                //ExcelUtility.ExportToExcel(dt, @"D:\Reliant.xlsx");
+                //DataSet ds = new DataSet();
+                //ds.Tables.Add(dt);
+                //ds.Tables[0].TableName = "REPORT";
+
+
+                ExcelUtility.WriteDataTableToExcel(dt, @"Reliant.xlsx");
+                Console.WriteLine("Total Records Are " + lstRS.Count() + System.Environment.NewLine);
+                Console.WriteLine("Total Time Taken " + timeSpan.Hours + ":" + timeSpan.Minutes + ":" + timeSpan.Seconds + ":" + timeSpan.Milliseconds);
+                //var x= elist.Descendants().Count();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ExcelUtility.WriteLog(ex, "MainFunction");
+                return false;
+            }
+
+        }
+
+
+        public static string lookUpPublication(string pubName)
+        {
+            string publication_id = string.Empty;
+            XElement Publication = XElement.Load(Environment.CurrentDirectory + "\\" + "PublicationLookUp.xml");
+            var x = (from el in Publication.Elements()
+                     where (string)el.Attribute("Title").Value == pubName
+                     select el.FirstAttribute).ToArray()[0].Value;
+
+            return x.ToString();
+        }
+
+        public static DataTable ToDataTable<T>(this IList<T> data)
+        {
+            PropertyDescriptorCollection props =
+                TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            for (int i = 0; i < props.Count; i++)
+            {
+                PropertyDescriptor prop = props[i];
+                table.Columns.Add(prop.Name, prop.PropertyType);
+            }
+            object[] values = new object[props.Count];
+            foreach (T item in data)
+            {
+                for (int i = 0; i < values.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item);
+                }
+                table.Rows.Add(values);
+            }
+            return table;
+        }
+
+    }
+
+}
